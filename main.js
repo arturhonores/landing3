78a056ce5d610fd4804305c974e931f67a7a6ba7
arturhonores <luis.arturhonores@gmail.com>
2023-10-22 14:25:25 +0200

    input section logic1

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+// referencing DOM elements
+const form = document.getElementById("form")
+const inputLinks = document.getElementById("inputLinks")
+
+function consoleMessage() {
+    console.log(inputLinks.value)
+}
+
+form.addEventListener("submit", e => {
+    e.preventDefault()
+    consoleMessage()
+})
 
